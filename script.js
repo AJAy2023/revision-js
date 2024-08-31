784ca40b8a5db4ad7f955fd1aada8d86cbd91ff4
“[firstname <ajayjadhav0914@gmailcom>
2024-09-01 00:12:10 +0530

    basic js

---
+++ script.js
@@ -0,0 +1,167 @@
+
+
+// array :  they are basically single objects that contain multiple values stored in a list.
+
+
+
+/* 
+syntax of array : 
+let shopping  = ["t-shirt", "shirt","pant","sadhi"];
+console.log(shopping)
+
+
+
+// ****    in javacsript array is homogineus and hetro ginus it means we can store both values like number and srting in array
+
+ex :
+let hitro = ["ajay",21,24,"jadhav"];
+console.log(hitro);
+
+
+why js is heterogeneous  bcz : 
+
+it is loosly- type nature 
+it means we it has flexibilty to store any type of value it may variables or string 
+
+let x = "string";
+x = 23; //  now x become number 
+
+
+
+// find the length of an array 
+
+let  shopping  = ["t-shirt" , "shirt", "pants" , 24];
+
+console.log(shopping.length);
+
+
+// in array we can acces and modify 
+ex
+ 1. Acess 
+let  shopping  = ["t-shirt" , "shirt", "pants" , 24];
+
+console.log(shopping[0]);
+console.log(shopping[3]);
+
+2. modify 
+let  shopping  = ["t-shirt" , "shirt", "pants" , 24];
+
+ shopping[3] = "ajay";
+ console.log(shopping);
+
+
+ array methods in js : - 
+ let array = [2,3,4,5,6];
+ array.push(8);
+//  console.log(array);
+
+ array.unshift(9);
+ console.log(array);
+
+
+
+ // ***    
+  // ACCESING THE EVERY ITOM
+
+ const birds = ["parots","picock","chimni"];
+
+ for(const bird of birds)
+ {
+    console.log(bird);
+ }
+
+
+ // **** 
+  // push()
+// function pushExample(arr, element) {
+//   console.log("Original Array:", arr);
+
+//   arr.push(element);
+//   console.log("After push:", arr);
+// }
+// pushExample([1, 2, 3], 4);
+ */
+
+
+
+
+
+
+
+// class , objecs , map , filter, reduce ,math , json , promises , 
+
+
+
+
+// // map()
+// function mapExample(arr) {
+//   console.log("Original Array:", arr);
+
+//   let newArr = arr.map(function(item) {
+//     return item * 2;
+//   });
+//   console.log("After map:", newArr);
+// }
+// mapExample([1, 2, 3]);
+
+
+// map() method is use to manipuate the data from original containt without changing the origianl 
+
+
+// let number  = [2,3,4,5];
+// let sqnumbers = number.map(user => user*user);
+// console.log(sqnumbers);
+
+
+// filter()
+// function filterExample(arr) {
+//   console.log("Original Array:", arr);
+
+//   let newArr = arr.filter(function(item) {
+//     return item > 2;
+//   });
+//   console.log("After filter:", newArr);
+// }
+// filterExample([1, 2, 3, 4, 5]);
+
+
+// let  number  = [2,4,5,7,8];
+
+// let newarray = number.filter((number)=>number%10 ===1);
+// console.log(newarray);
+
+
+
+// // substring
+// function getSubstring(str, start, end) {
+//   console.log("Original String:", str);
+//   console.log("After substring:", str.substring(start, end));
+// }
+
+// getSubstring("Hello World", 0, 5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
